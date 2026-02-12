9943cd3df55882f8ce3c3a04ffe15a26321e5dc9
Akshit Bansal <bansalakshitwork@gmail.com>
2026-02-12 22:59:53 +0530

    Initial Commit

---
+++ src/test/java/io/github/abansal755/spring_query/SpringQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.abansal755.spring_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
