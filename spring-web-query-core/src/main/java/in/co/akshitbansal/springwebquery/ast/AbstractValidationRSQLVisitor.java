5d1e9a8ec75ce323985e4b27001f5194f66d81d3
Akshit Bansal <bansalakshitwork@gmail.com>
2026-04-20 12:23:02 +0530

    Added exceptions `QueryForbiddenLogicalOperatorException` and `QueryMaxASTDepthExceededException`

--- spring-web-query-core/src/main/java/in/co/akshitbansal/springwebquery/ast/AbstractValidationRSQLVisitor.java
+++ spring-web-query-core/src/main/java/in/co/akshitbansal/springwebquery/ast/AbstractValidationRSQLVisitor.java
@@ -17,6 +17,8 @@
 package in.co.akshitbansal.springwebquery.ast;
 
 import cz.jirutka.rsql.parser.ast.*;
+import in.co.akshitbansal.springwebquery.exception.QueryForbiddenLogicalOperatorException;
+import in.co.akshitbansal.springwebquery.exception.QueryMaxASTDepthExceededException;
 import in.co.akshitbansal.springwebquery.exception.QueryValidationException;
 import in.co.akshitbansal.springwebquery.resolver.field.FieldResolver;
 import in.co.akshitbansal.springwebquery.validator.FilterableFieldValidator;
@@ -143,16 +145,16 @@ public abstract class AbstractValidationRSQLVisitor implements RSQLVisitor<Void,
 	 * @throws QueryValidationException if an operator is disallowed or the depth exceeds the limit
 	 */
 	protected void validateNode(Node node, NodeMetadata metadata) {
-		if ((node instanceof AndNode) && !andNodeAllowed)
-			throw new QueryValidationException("Logical AND operator is not allowed");
-		if ((node instanceof OrNode) && !orNodeAllowed)
-			throw new QueryValidationException("Logical OR operator is not allowed");
+		if ((node instanceof AndNode andNode) && !andNodeAllowed)
+			throw new QueryForbiddenLogicalOperatorException("Logical AND operator is not allowed", andNode.getOperator());
+		if ((node instanceof OrNode orNode) && !orNodeAllowed)
+			throw new QueryForbiddenLogicalOperatorException("Logical OR operator is not allowed", orNode.getOperator());
 		int depth = metadata.getDepth();
 		if (depth > maxDepth) {
-			throw new QueryValidationException(MessageFormat.format(
+			throw new QueryMaxASTDepthExceededException(MessageFormat.format(
 					"Exceeded maximum allowed depth of RSQL Abstract Syntax Tree ({0}) at node: {1}",
 					maxDepth, node
-			));
+			), maxDepth);
 		}
 	}
 }
---
+++ spring-web-query-core/src/main/java/in/co/akshitbansal/springwebquery/exception/QueryForbiddenLogicalOperatorException.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2026-present Akshit Bansal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package in.co.akshitbansal.springwebquery.exception;
+
+import cz.jirutka.rsql.parser.ast.LogicalOperator;
+import lombok.Getter;
+
+@Getter
+public class QueryForbiddenLogicalOperatorException extends QueryValidationException {
+
+	private final LogicalOperator logicalOperator;
+
+	public QueryForbiddenLogicalOperatorException(String message, LogicalOperator logicalOperator) {
+		super(message);
+		this.logicalOperator = logicalOperator;
+	}
+
+	public QueryForbiddenLogicalOperatorException(String message, LogicalOperator logicalOperator, Throwable cause) {
+		super(message, cause);
+		this.logicalOperator = logicalOperator;
+	}
+}
