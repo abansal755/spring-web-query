8d1ae59c4ddbc02533ef94916b0500b9eb32f180
Akshit Bansal <bansalakshitwork@gmail.com>
2026-04-24 23:54:07 +0530

    Removed unused imports

--- spring-web-query-core/src/main/java/in/co/akshitbansal/springwebquery/repository/WebQueryRepository.java
+++ spring-web-query-core/src/main/java/in/co/akshitbansal/springwebquery/repository/WebQueryRepository.java
@@ -30,7 +30,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Collections;
 import java.util.List;
 
 /**
