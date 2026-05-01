623e18240c2c11f6ddc21a0af0cc5c86b4d5dd50
Akshit Bansal <bansalakshitwork@gmail.com>
2026-05-02 01:50:47 +0530

    Added unit tests for `TupleConverterFactory`

---
+++ spring-web-query-core/src/test/java/in/co/akshitbansal/springwebquery/tupleconverter/TupleConverterFactoryTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2026-present Akshit Bansal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package in.co.akshitbansal.springwebquery.tupleconverter;
+
+import in.co.akshitbansal.springwebquery.model.Address;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class TupleConverterFactoryTest {
+
+	@Test
+	void testConstructionWithNullDiscoverer() {
+		assertThrows(NullPointerException.class, () -> new TupleConverterFactory(null));
+	}
+
+	@Test
+	void testConstructionWithNonNullDiscoverer() {
+		var discovererFactory = new PreferredConstructorDiscovererFactory(false);
+		assertDoesNotThrow(() -> new TupleConverterFactory(discovererFactory));
+	}
+
+	@Test
+	void testNewConverterWithNullClass() {
+		var factory = new TupleConverterFactory(new PreferredConstructorDiscovererFactory(false));
+		assertThrows(NullPointerException.class, () -> factory.newConverter(null));
+	}
+
+	@Test
+	void testNewConverterWithNonNullClass() {
+		var factory = new TupleConverterFactory(new PreferredConstructorDiscovererFactory(false));
+		assertDoesNotThrow(() -> factory.newConverter(Address.class));
+	}
+}
